2bc1b5a21116be211d93e4cdf219a4e6b66a7170
Mark <altosaar@gmail.com>
2020-12-09 14:57:45 -0500

    Revert "Update start.sh"
    This reverts commit bc7de2cf4961dadf05a8028bc11f863081f6498c.

--- start.sh
+++ start.sh
@@ -8,8 +8,8 @@ else
     exit 1
 fi
 
-# 2. Create the folders if it does not exist
-mkdir -p data data/limesurvey/source
+# 2. Create the data folder if it does not exist
+mkdir -p data
 
 # 3. Download the specified version of limesurvey
 curl -sSL https://github.com/LimeSurvey/LimeSurvey/archive/$VERSION.tar.gz --output ./data/limesurvey.tar.gz
@@ -17,28 +17,28 @@ curl -sSL https://github.com/LimeSurvey/LimeSurvey/archive/$VERSION.tar.gz --out
 # 4. Extract the limesurvey executable
 tar xzvf ./data/limesurvey.tar.gz --strip-components=1 -C ./data/limesurvey/source/
 
-# 5. Remove the archive and other files not needed
-rm -rf ./data/limesurvey.tar.gz ./data/limesurvey/source/docs ./data/limesurvey/source/tests ./data/limesurvey/source/*.md
+# # 5. Remove the archive and other files not needed
+# rm -rf ./data/limesurvey.tar.gz ./data/limesurvey/source/docs ./data/limesurvey/source/tests ./data/limesurvey/source/*.md
 
-# 6. Create the directory for storing the limesurvey runtime files
-mkdir -p ./data/limesurvey/runtime
+# # 6. Create the directory for storing the limesurvey runtime files
+# mkdir -p ./data/limesurvey/runtime
 
-# 7. Download plugins
-wget http://extensions.sondages.pro/IMG/auto/addScriptToQuestion.zip -P ./data/limesurvey/source/plugins/
-unzip ./data/limesurvey/source/plugins/addScriptToQuestion.zip -d ./data/limesurvey/source/plugins/
-rm ./data/limesurvey/source/plugins/addScriptToQuestion.zip
+# # 7. Download plugins
+# wget http://extensions.sondages.pro/IMG/auto/addScriptToQuestion.zip -P ./data/limesurvey/source/plugins/
+# unzip ./data/limesurvey/source/plugins/addScriptToQuestion.zip -d ./data/limesurvey/source/plugins/
+# rm ./data/limesurvey/source/plugins/addScriptToQuestion.zip
 
-# 8. Set permissions
-chown -R www-data:www-data ./data/limesurvey/;
-find ./data/limesurvey/source/ -type d -exec chmod 755 {} \;
-find ./data/limesurvey/source/ -type f -exec chmod 644 {} \;
-find ./data/limesurvey/source/plugins/ -type d -exec chmod 750 {} \;
-find ./data/limesurvey/source/plugins/ -type f -exec chmod 640 {} \;
+# # 8. Set permissions
+# chown -R www-data:www-data ./data/limesurvey/;
+# find ./data/limesurvey/source/ -type d -exec chmod 755 {} \;
+# find ./data/limesurvey/source/ -type f -exec chmod 644 {} \;
+# find ./data/limesurvey/source/plugins/ -type d -exec chmod 750 {} \;
+# find ./data/limesurvey/source/plugins/ -type f -exec chmod 640 {} \;
 
-# 9. Update local images
-docker-compose pull
+# # 9. Update local images
+# docker-compose pull
 
-# 10. Start limesurvey
-docker-compose up -d
+# # 10. Start limesurvey
+# docker-compose up -d
 
 exit 0
